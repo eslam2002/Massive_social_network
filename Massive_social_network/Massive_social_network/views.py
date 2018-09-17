a9a27db21a42fca64dbbd14bc2e7c74e3236b43c
Eslam Sam <es2002@protonmail.ch>
2018-09-17 01:13:50 +0100

    commit

--- Massive_social_network/Massive_social_network/views.py
+++ Massive_social_network/Massive_social_network/views.py
@@ -2,16 +2,24 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
+from posts.models import Post
 
 User = get_user_model()
 
 class SearchView(View):
+
     def get(self, request, *args, **kwargs):
         query = request.GET.get("q")
-        qs = None
-        if query:
-            qs = User.objects.filter(
-                    Q(username__icontains=query)
+        posts = Q(content__icontains=query)
+        users = Q(username__icontains=query)
+        qsUsers = None
+        qsPosts = None
+        if query:                                          #|
+            qsUsers = User.objects.filter(
+                 users
                 )
-        context = {"users": qs}
+            qsPosts = Post.objects.filter(
+                posts
+                )
+        context = {"users": qsUsers,"posts":qsPosts}
         return render(request, "posts/search.html", context)
---
+++ README.md
@@ -0,0 +1,1 @@
+# Massive_social_network
